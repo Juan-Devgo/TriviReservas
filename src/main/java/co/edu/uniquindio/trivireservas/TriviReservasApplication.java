22484d0e3d4f7e51508347711369fb8b14ce3732
Juan-Devgo <juand.garcian@uqvirtual.edu.co>
2025-08-17 11:34:21 -0500

    0: Creación de la estructura del proyecto

---
+++ src/main/java/co/edu/uniquindio/trivireservas/TriviReservasApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.trivireservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviReservasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriviReservasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/uniquindio/trivireservas/TriviReservasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.trivireservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviReservasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
